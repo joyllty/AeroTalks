9f1ff547b55c96c92331e61206527fb424fe0d3d
Rebecca Beccari <rebeccabf06@gmail.com>
2026-06-03 17:10:48 -0300

    Implementação de interface básica

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,9 @@
-'''
-pacote de rotas da aplicação.
-'''
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    from app.routes.test_routes import health_bp
+    app.register_blueprint(health_bp)
+
+    return app
--- app/routes/test_routes.py
+++ app/routes/test_routes.py
@@ -1,32 +1,11 @@
-'''
-rotas básicas/ de teste pra ver se a API está funcionando
-'''
+from flask import Blueprint, jsonify, render_template
 
-# Blueprint permite separar as rotas em arquivos diferentes
-from flask import Blueprint, jsonify
-
-# cria um grupo de rotas chamado "health"
-# o primeiro argumento é o nome interno do Blueprint
-# o segundo argumento (__name__) ajuda o Flask a localizar esse arquivo
 health_bp = Blueprint("health", __name__)
 
-
-# define uma rota GET para a raiz do servidor
 @health_bp.route("/", methods=["GET"])
 def home():
-    '''
-    rota inicial da API
-    serve apenas para verificar se o servidor Flask está respondendo
-    '''
-    return jsonify({'message': 'API do chat funcionando!'}), 200
+    return render_template('index.html')
 
-
-# define uma rota GET para verificar o status da API
 @health_bp.route("/api/health", methods=["GET"])
 def health_check():
-    '''
-    rota de status da API
-
-    se essa rota responder com "online", significa que a aplicação Flask está rodando
-    '''
     return jsonify({'status': 'online'}), 200
